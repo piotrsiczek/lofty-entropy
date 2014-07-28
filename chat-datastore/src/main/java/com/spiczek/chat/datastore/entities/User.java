e65d067b2b9dcdd1cee277b5a62b854b93f957f5
Piotr Siczek <spiczek@gmail.com>
2014-07-28 20:25:04 +0200

    jpa dependencies

---
+++ chat-datastore/src/main/java/com/spiczek/chat/datastore/entities/User.java
@@ -0,0 +1,46 @@
+package com.spiczek.chat.datastore.entities;
+
+import com.google.appengine.api.datastore.Key;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by piotr on 2014-07-27.
+ */
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Key id;
+
+    private String name;
+    private String surname;
+
+    public Key getId() {
+        return id;
+    }
+
+    public void setId(Key id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
