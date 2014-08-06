14095c83c6debccc8904598c7684874ed96274e0
Piotr Siczek <spiczek@gmail.com>
2014-08-06 10:18:30 +0200

    backend refactoring

--- chat-backend/src/main/java/com/spiczek/chat/backend/service/ClientServiceImpl.java
+++ chat-backend/src/main/java/com/spiczek/chat/backend/service/ClientServiceImpl.java
@@ -1,11 +1,16 @@
 package com.spiczek.chat.backend.service;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.spiczek.chat.datastore.entities.DAO;
+import com.spiczek.chat.datastore.entities.User;
 import com.spiczek.chat.shared.ClientService;
 
 public class ClientServiceImpl extends RemoteServiceServlet implements ClientService {
-    // Implementation of sample interface method
     public String getMessage(String msg) {
-      return "Client said: \"" + msg + "\"<br>Server answered: \"Hi!\"";
+
+        DAO d = new DAO();
+        User u = d.createUser();
+
+      return "Send from client " + msg + " " + u.getName() + " " + u.getId() + " " + u.getSurname();
     }
 }
