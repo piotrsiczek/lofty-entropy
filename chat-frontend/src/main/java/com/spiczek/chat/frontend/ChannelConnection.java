e46f5f059b70ac6d89a6d02221699d53d637ce79
Piotr Siczek <spiczek@gmail.com>
2014-08-08 22:46:59 +0200

    javascript logging

--- chat-frontend/src/main/java/com/spiczek/chat/frontend/ChannelConnection.java
+++ chat-frontend/src/main/java/com/spiczek/chat/frontend/ChannelConnection.java
@@ -1,11 +1,12 @@
 package com.spiczek.chat.frontend;
 
+import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.event.shared.EventBus;
-import com.google.gwt.user.client.Window;
 import com.spiczek.chat.frontend.events.MessageReceivedEvent;
 
+
 /**
- * Created by piotr on 2014-04-26.
+ * @author Piotr Siczek
  */
 public class ChannelConnection {
 
@@ -13,48 +14,60 @@ public class ChannelConnection {
     private static EventBus eventBus;
 
     public ChannelConnection(String token, EventBus eventBus) {
-
+        Log.info("ChannelConnection constructor");
         this.token = token;
         this.eventBus = eventBus;
         //eventBinder.bindEventHandlers(this, eventBus);
-        System.out.println("&&&&&&&&&&&&&&&&&&&&");
-        this.connect(token);
         this.publish();
-        System.out.println("&&&&&&&&&&&&&&&&&&&&");
+        this.connect(token);
+
     }
 
     private native void publish() /*-{
-        $wnd.alert("publish");
+
+       this.@com.spiczek.chat.frontend.ChannelConnection::log(Ljava/lang/String;)("published log");
+//        alert("asdf");
+        $wnd.log = @com.spiczek.chat.frontend.ChannelConnection::log(Ljava/lang/String;);
+        $wnd.log("publish");
+        //$wnd.alert("publish");
         $wnd.onOpen = @com.spiczek.chat.frontend.ChannelConnection::onOpen();
         $wnd.onClose = @com.spiczek.chat.frontend.ChannelConnection::onClose();
         $wnd.onMessage = @com.spiczek.chat.frontend.ChannelConnection::onMessage(Ljava/lang/String;);
         $wnd.onError = @com.spiczek.chat.frontend.ChannelConnection::onError(Ljava/lang/String;I);
-        $wnd.alert("end publish");
+        $wnd.log("end publish");
+        //$wnd.alert("end publish");
     }-*/;
 
     private native void connect(String token) /*-{
-
-        $wnd.alert(token);
+        //$wnd.alert(token);
+        $wnd.log("token: " + token);
         $wnd.connectChannel(token);
 
     }-*/;
 
     public static void onOpen() {
-        Window.alert("open from java");
+        log("onOpen from java");
+        //Window.alert("open from java");
     }
 
     public static void onMessage(String message) {
         String[] parts = message.split(";");
-        Window.alert("message from java: " + parts[0] + " data: " + parts[1]);
+        log("message from java: " + parts[0] + " data: " + parts[1]);
+        //Window.alert("message from java: " + parts[0] + " data: " + parts[1]);
         eventBus.fireEvent(new MessageReceivedEvent(new Message(Integer.parseInt(parts[0]), parts[1])));
     }
 
     public static void onClose() {
-        Window.alert("close from java");
+        log("onClose from java");
+        //Window.alert("close from java");
     }
 
     public static void onError(String description, int code) {
-        Window.alert("error from java " + description + " " + code);
+        log("error from java " + description + " " + code);
+        //Window.alert("error from java " + description + " " + code);
     }
 
+    public static void log(String data) {
+        Log.info(data);
+    }
 }
