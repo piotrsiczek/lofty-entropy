c1cba864db1818f38c53d17c69f5b2e08b517a91
Piotr Siczek <spiczek@gmail.com>
2014-09-07 18:51:40 +0200

    MessageComposite refactoring

--- chat-frontend/src/main/java/com/spiczek/chat/frontend/composites/xarchives/messages/ArchiveMessageComposite.java
+++ chat-frontend/src/main/java/com/spiczek/chat/frontend/composites/xarchives/messages/ArchiveMessageComposite.java
@@ -1,11 +1,18 @@
 package com.spiczek.chat.frontend.composites.xarchives.messages;
 
+import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.web.bindery.event.shared.binder.EventBinder;
 import com.spiczek.chat.frontend.composites.messages.MessagePanel;
+import com.spiczek.chat.frontend.events.CompositeCloseEvent;
 import com.spiczek.chat.shared.dto.MessageDTO;
 import com.spiczek.chat.shared.dto.UserDTO;
 
@@ -18,13 +25,20 @@ public class ArchiveMessageComposite extends Composite {
     interface ArchiveMessageUiBinder extends UiBinder<HTMLPanel, ArchiveMessageComposite> {}
     private static ArchiveMessageUiBinder uiBinder = GWT.create(ArchiveMessageUiBinder.class);
 
-    @UiField(provided=true) MessagePanel messagePanel;
+    interface MyEventBinder extends EventBinder<ArchiveMessageComposite> {}
+    private final MyEventBinder eventBinder = GWT.create(MyEventBinder.class);
 
+    @UiField(provided=true) MessagePanel messagePanel;
+    @UiField Button closeButton;
+
+    private EventBus eventBus;
     private UserDTO user;
 
-    public ArchiveMessageComposite(UserDTO user, List<MessageDTO> messages) {
+    public ArchiveMessageComposite(EventBus eventBus, UserDTO user, List<MessageDTO> messages) {
         messagePanel = new MessagePanel(user.getLogin(), "cus");
         this.initWidget(uiBinder.createAndBindUi(this));
+        eventBinder.bindEventHandlers(this, eventBus);
+        this.eventBus = eventBus;
 
         this.user = user;
         initialize(messages);
@@ -39,5 +53,11 @@ public class ArchiveMessageComposite extends Composite {
                 messagePanel.createRightMessage(m.getText(), "time");
             }
         }
+    }
+
+    @UiHandler("closeButton")
+    public void onCloseButtonCliced(ClickEvent e) {
+        Log.info("cliced");
+        eventBus.fireEvent(new CompositeCloseEvent(this));
     }
 }
