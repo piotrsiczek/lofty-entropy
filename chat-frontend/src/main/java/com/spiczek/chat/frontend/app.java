bb49aa9786147fd4d8e6a6991f8214b17361f331
Piotr Siczek <spiczek@gmail.com>
2014-09-21 14:39:51 +0200

    LE-33 Login form design

--- chat-frontend/src/main/java/com/spiczek/chat/frontend/app.java
+++ chat-frontend/src/main/java/com/spiczek/chat/frontend/app.java
@@ -7,8 +7,6 @@ import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.spiczek.chat.frontend.composites.panels.VPanel;
 import com.spiczek.chat.frontend.composites.talks.TalkComposite;
 import com.spiczek.chat.frontend.composites.friends.FriendPanelRenderer;
 import com.spiczek.chat.frontend.composites.widgets.listpanel.ListPanel;
@@ -27,191 +25,18 @@ public class app implements EntryPoint {
 
     private final ClientServiceAsync clientService = GWT.create(ClientService.class);
     private final MessageServiceAsync messageService = GWT.create(MessageService.class);
-    private TextBox friendIdBox;
     private SimpleEventBus eventBus;
-    private ListPanel fr;
 
-//    private TalkComposite talkComposite;
     /**
      * This is the entry point method.`
      */
     public void onModuleLoad() {
-
-        //fr = new FriendComposite();
-
-        //RootPanel.get("slot3").add(fr);
+        eventBus = new SimpleEventBus();
 
         initWidgets();
-
-        //fr.initialize();
-//        userService.friends("dupa", new AsyncCallback<String>() {
-//            @Override
-//            public void onFailure(Throwable caught) {
-//                Log.error(caught.toString());
-//            }
-//
-//            @Override
-//            public void onSuccess(String result) {
-//                Log.info(result);
-//            }
-//        });
-
-//        clientService.generateFriends(new AsyncCallback<Void>() {
-//            @Override
-//            public void onFailure(Throwable throwable) {
-//                Log.error("dupa");
-//            }
-//
-//            @Override
-//            public void onSuccess(Void v) {
-//                Log.info("Successfylly generated friendsdeprecated.");
-//            }
-//        });
-//
-//        for (int i=0; i<1000000000;i++) {
-//
-//        }
-
-//        final Button generateTestButton = new Button("Click me");
-//        final Label label = new Label();
-
-
-//        final MessageComposite messageComposite = new MessageComposite(eventBus, 9, 9);
-
-//        generateTestButton.addClickHandler(new ClickHandler() {
-//            public void onClick(ClickEvent event) {
-//                if (label.getText().equals("")) {
-//                    clientService.getMessage("Hello, World!", new AsyncCallback<String>() {
-//                        @Override
-//                        public void onFailure(Throwable caught) {
-//                            label.setText("Failed to receive answer from server!");
-//                        }
-//
-//                        @Override
-//                        public void onSuccess(String result) {
-//                            label.getElement().setInnerHTML(result);
-//                        }
-//                    });
-//                } else {
-//                    label.setText("");
-//                }
-//            }
-//        });
-
-
-
-        Button generateFrinedsButton = new Button("generate friendsdeprecated");
-        Button getFrinedsButton = new Button("get friendsdeprecated");
-
-        final VPanel friendVPanel = new VPanel(generateFrinedsButton, getFrinedsButton);
-
-//        generateFrinedsButton.addClickHandler(new ClickHandler() {
-//            @Override
-//            public void onClick(ClickEvent clickEvent) {
-//                clientService.generateFriends(new AsyncCallback<Void>() {
-//                    @Override
-//                    public void onFailure(Throwable throwable) {
-//                        Log.error("dupa");
-//                    }
-//
-//                    @Override
-//                    public void onSuccess(Void v) {
-//                        Log.info("Successfylly generated friendsdeprecated.");
-//                    }
-//                });
-//            }
-//        });
-
-        //FriendCell friendCell = new FriendCell();
-        //final CellList<UserDTO> cellList = new CellList<UserDTO>(friendCell);
-        //TextCell textCell = new TextCell();
-        //final CellList<String> cellList = new CellList<String>(textCell);
-
-//        friendsList.setKeyboardSelectionPolicy(HasKeyboardSelectionPolicy.KeyboardSelectionPolicy.ENABLED);
-//        // Add a selection model to handle user selection.
-//        final SingleSelectionModel<FriendInfo> selectionModel = new SingleSelectionModel<FriendInfo>();
-//        friendsList.setSelectionModel(selectionModel);
-//        selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
-//            public void onSelectionChange(SelectionChangeEvent event) {
-//                FriendInfo selected = selectionModel.getSelectedObject();
-//                if (selected != null) {
-//                    Window.alert("You selected: " + selected.getName());
-//                }
-//            }
-//        });
-
-
-
-
-
-        // Set a key provider that provides a unique key for each contact. If key is
-        // used to identify contacts when fields (such as the name and address)
-        // change.
-
-//        cellList.setPageSize(10);
-//        cellList.setKeyboardPagingPolicy(HasKeyboardPagingPolicy.KeyboardPagingPolicy.INCREASE_RANGE);
-//        cellList.setKeyboardSelectionPolicy(HasKeyboardSelectionPolicy.KeyboardSelectionPolicy.BOUND_TO_SELECTION);
-//
-//        // Add a selection model so we can select cells.
-//        final SingleSelectionModel<UserDTO> selectionModel = new SingleSelectionModel<UserDTO>();
-//        cellList.setSelectionModel(selectionModel);
-//        selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
-//            public void onSelectionChange(SelectionChangeEvent event) {
-//                //contactForm.setContact(selectionModel.getSelectedObject());
-//            }
-//        });
-
-
-
-
-
-        //PagerPanel pagerPanel = new PagerPanel();
-        //pagerPanel.setDisplay(cellList);
-
-
-        //cellList.setRowCount(100, false);
-        //ScrollPanel scroll = new ScrollPanel(cellList);
-
-        //friendVPanel.add(scroll);
-
-        // Push the data into the widget.
-
-
-//        getFrinedsButton.addClickHandler(new ClickHandler() {
-//            @Override
-//            public void onClick(ClickEvent clickEvent) {
-//                clientService.getFriends(new AsyncCallback<List<UserDTO>>() {
-//                    @Override
-//                    public void onFailure(Throwable throwable) {
-//                        Log.error("dupa2");
-//                    }
-//
-//                    @Override
-//                    public void onSuccess(List<UserDTO> userDTOs) {
-//                        Log.info("get data");
-//                        List<String> items = new ArrayList<String>();
-////                        for (UserDTO u : userDTOs) {
-//////                            Label id = new Label(u.getId() + "");
-//////                            Label name = new Label(u.getName());
-//////                            Label surname = new Label(u.getSurname());
-//////                            HPanel friendHPanel = new HPanel(id, name, surname);
-//////                            friendVPanel.add(friendHPanel);
-////                            items.add(u.getId() + " " + u.getName() + " " + u.getSurname());
-////                        }
-//                        cellList.setRowData(0, userDTOs);
-//
-//                        Log.info("data displayed");
-//                    }
-//                });
-//            }
-//        });
-
-        //RootPanel.get("slot3").add(new FriendComposite());
     }
 
     private void initWidgets() {
-        eventBus = new SimpleEventBus();
-
         clientService.getUserDetails(new AsyncCallback<UserDTO>() {
             @Override
             public void onFailure(Throwable caught) {
